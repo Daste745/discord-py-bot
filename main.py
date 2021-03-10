f45936bea77114e74596e1ff57e76655a480b643
Daste <stefankar1000@gmail.com>
2021-03-10 18:29:13 +0100

    SIGTERM handler
    Handles the SIGTERM signal sent by Docker and gracefully shuts everything down.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import signal
 
 import asyncio
 from discord import Intents, AllowedMentions
@@ -20,12 +21,18 @@ bot = Bot(
 )
 
 
+def sigterm_handler(signum, frame):
+    log.info("Received SIGTERM, exiting gracefully...")
+    raise KeyboardInterrupt
+
+
 if __name__ == "__main__":
     # Basic event loop, logs the bot out when control-c is pressed
+    signal.signal(signal.SIGTERM, sigterm_handler)
     loop = asyncio.get_event_loop()
     try:
         loop.run_until_complete(bot.start(os.getenv("BOT_TOKEN")))
     except KeyboardInterrupt:
         loop.run_until_complete(bot.logout())
     finally:
-        loop.run_until_complete(loop.close())
+        loop.close()
